d736d4eeab15e5cffeccb23cf88417e99f3605b9
pedro <pedrinho122.freitas@gmail.com>
2025-07-08 13:02:44 -0300

    Relacionamentos feito

---
+++ database/migrations/2025_07_08_142401_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+use App\Models\Category;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->dateTime('due_date');
+            $table->foreignIdFor(User::class)->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId(Category::class)->references('id')->on('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['category_id']);
+        });
+        Schema::dropIfExists('tasks');
+    }
+};
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Pedro',
+            'email' => 'pedro@gmail.com',
+            'password' => Hash::make('12345678')
+        ]);
+    }
+}
